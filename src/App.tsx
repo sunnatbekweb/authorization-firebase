caaf5accf4606aaf0dc820a5d8a14916c4741a27
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-11-26 13:52:18 +0500

    created user authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
 
+// interface RegisterData {
+//   email: string;
+//   password: string;
+// }
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 function App() {
-  const [userData, setUserData] = useState();
+  const [userData, setUserData] = useState<LoginData>({
+    email: "",
+    password: "",
+  });
+
+  const handleRegChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(userData);
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
+      .then((userCredential) => {
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>Auth</h1>
-      <form>
-        <input type="email" />
-        <input type="password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          value={userData.email}
+          onChange={handleRegChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={userData.password}
+          onChange={handleRegChange}
+        />
         <button>Sing Up</button>
       </form>
     </div>
